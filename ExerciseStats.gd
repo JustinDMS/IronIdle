c58b39041ef2b73652602aaca868e3d7a55769ec
JustinDMS <90155396+JustinDMS@users.noreply.github.com>
2023-01-05 11:41:13 -0600

    Restructured Project Files
    Added folders for respective file types (scripts, scenes, fonts, art, etc)

--- ExerciseStats.gd
+++
@@ -1,8 +0,0 @@
-extends Resource
-
-export var exercise_name : String
-export(String, "Calisthenic", "Weighted") var exercise_type
-export var rep_time : float
-export(String, "Chest", "Shoulders", "Back", "Core", "Quadriceps", "Hamstrings") var muscle_groups
-export(String, "None", "High Bar", "Bench", "Rack", "Deadlift Platform") var gym_req
-export(String, "None", "Dumbbell", "Plates") var equipment_req
